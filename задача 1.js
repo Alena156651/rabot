ae70e92be04d7e7d547609139d7a11fbd95f6362
Alena156651 <81128577+Alena156651@users.noreply.github.com>
2021-03-31 19:54:52 +0700

    Add files via upload

---
+++ задача 1.js
@@ -0,0 +1,44 @@
+Number{
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+movies: {
+    "logan": "Я посмотрела 10 фильмов"
+    }
+personalMovieDB{
+    count: numberOfFilms;
+    movies: {};
+    actors: {};
+    genres: [];
+    privat: false;
+    } 
+count{
+    a = print("Один из последних просмотренных фильмов?");
+    b = print("На сколько оцените его?");
+    movies: {
+        "logan":"Последний просмотренный фильм Гарри Поттер"
+        "logan":"Оценю на 10 баллов из 10"
+    }
+    }
+count{
+    q = print("Один из последних просмотренных фильмов?");
+    w = print("На сколько оцените его?");
+    movies: {
+        "logan":"Последний просмотренный фильм Гарри Поттер"
+        "logan":"Оценю на 10 баллов из 10"
+    }
+    }   
+while (personalMovieDB , private != false || (c = 0 || c > 50)){
+    console.log(personalMovieDB)
+    }
+if (personalMovieDB.count < 10) {
+     log("Просмотрено довольно мало фильмов");
+    } 
+else if (personalMovieDB.count > 10 n personalMovieDB.count < 30) {
+    log("Вы классический зритель");
+    } 
+else if (personalMovieDB.count > 30) {
+    log("Вы киноман");
+    } 
+else{
+    log("Произошла ошибка");
+    }
+}       
